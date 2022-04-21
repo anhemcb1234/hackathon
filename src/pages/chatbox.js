034c5882038ad6c22895701e0941c9b35675c224
HOANG TRUNG TIEN <hoangtrungtien99@gmail.com>
2022-04-21 22:56:18 +0700

    Done

--- src/pages/chatbox.js
+++ src/pages/chatbox.js
@@ -1,6 +1,5 @@
 import {userServices} from '../services/userServices';
 import {useEffect, useState} from 'react'
-import { Link } from "react-router-dom";
 import {
     useNavigate,
   } from "react-router-dom";
